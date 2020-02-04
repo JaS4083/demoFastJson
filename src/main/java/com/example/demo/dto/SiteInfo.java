b9c8586794aa463be58e0a22f2b15032ea9733e7
Artem Sirobaba <artemsi@wizardsdev.com>
2020-02-04 11:49:56 +0200

    small fix

--- src/main/java/com/example/demo/dto/SiteInfo.java
+++ src/main/java/com/example/demo/dto/SiteInfo.java
@@ -8,19 +8,6 @@ import lombok.ToString;
 
 import java.util.List;
 
-/**
- * id - Site ID on the exchange.
- * name - Site name. Usually in this attribute publisher’s domain will be passed.
- * domain - ? // need to check Open RTB spec.
- * keyWords - ? // need to check Open RTB spec.
- * page - Full url of the page where impression to be occur.
- * ref - ? // need to check Open RTB spec.
- * search - ? // need to check Open RTB spec.
- * publisher - Publisher Object.
- * ext - Optional site attributes, such as section ID and alternative categorization.
- * cat - Array of IAB content categories for the overall site.
- */
-
 @ToString
 @Getter
 @AllArgsConstructor
